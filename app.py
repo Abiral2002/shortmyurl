2e981a4314cffbafd71d3cd01dbe3cf27cdf5bd1
Bhawesh Chaudhary <callme.bhawesh@gmail.com>
2022-04-24 22:16:07 +0530

    few changes

--- app.py
+++ app.py
@@ -19,6 +19,18 @@ def reset():
 def register():
     return render_template('register.html', name='register')
 
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name='dashboard')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', name='about')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', name='contact')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
